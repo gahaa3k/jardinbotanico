d72517a895a40332e1ffe6b87dcf7173c2fbaea1
victor <victor@localhost.localdomain>
2025-04-06 00:58:52 +0200

    Hasta el menu de selección principal

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Holaa")
+print("Bienvenido a Mi Jardin Botanico!!")
+print("Que quieres hacer?")
+
+print("1: Mostrar información sobre plantas")
+print("2: Acceder al jardín")
+
+
+opcion = int(input(""))
+if opcion == 1:
+    print("Información sobre plantas")
+elif opcion == 2:
+    print("Bienvenido a tu jardin!!")
+else:
+    print("Introduce una opción valida!!!")
+
+
+
