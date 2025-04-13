bfb89c4155a1f0c202a1355b8a33e7c3009da573
victor <victor@localhost.localdomain>
2025-04-13 23:09:12 +0200

    na y menos

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ if opcion == 1:
     print("Información sobre plantas")
 elif opcion == 2:
     print("Bienvenido a tu jardin!!")
+    print("Quieres agregar alguna planta o mostrar información de las ya plantadas?")
+
+    print("1: Añadir planta")
+    print("2: Mostrar info de plantas")
+    opcion1 = int(input(""))
+    if opcion1 == 1:
+        print("SQLL")
+    elif opcion1 == 2:
+        print("SQLL")
+    else:
+        print("Introduce una opcion valida!!")
 else:
     print("Introduce una opción valida!!!")
     ##Actualización!!!!!
